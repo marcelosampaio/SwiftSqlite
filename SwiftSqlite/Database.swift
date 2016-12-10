7039ba241d93d204d0925859903c30c8b8fd0756
Marcelo Sampaio <marcelo@artlogica.com.br>
2016-12-09 23:32:00 -0200

    added bridging header :) 👍

---
+++ SwiftSqlite/Database.swift
@@ -0,0 +1,30 @@
+//
+//  Database.swift
+//  SwiftSqlite
+//
+//  Created by Marcelo Sampaio on 12/9/16.
+//  Copyright © 2016 Marcelo Sampaio. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+
+class Database: NSObject {
+    
+    
+    
+    var database : OpaquePointer? = nil
+    var writableDBPath = String()
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ SwiftSqlite/SwiftSqlite-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  SwiftSqlite-Bridging-Header.h
+//  SwiftSqlite
+//
+//  Created by Marcelo Sampaio on 12/9/16.
+//  Copyright © 2016 Marcelo Sampaio. All rights reserved.
+//
+
+#ifndef SwiftSqlite_Bridging_Header_h
+#define SwiftSqlite_Bridging_Header_h
+
+
+#endif /* SwiftSqlite_Bridging_Header_h */
+
+#import <sqlite3.h>
+
